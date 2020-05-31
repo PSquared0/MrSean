5008971d55312309726b9a181339207e9e018605
Pascale Pierre <pascaleee@gmail.com>
2020-05-31 18:38:25 -0500

    filler copy and box design nnav bar info added

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -5,7 +5,7 @@ class Nav extends Component {
     render() {
       return (
         <nav class="navbar">
-        <a href="#" class="logo">logo</a>
+        <a href="#" class="logo">Mr Seans Music</a>
         <ul class="main-nav" id="js-menu">
             <li>
                 <a href="#" class="nav-links">Home</a>
@@ -14,10 +14,10 @@ class Nav extends Component {
                 <a href="#" class="nav-links">Products</a>
             </li>
             <li>
-                <a href="#" class="nav-links">About Us</a>
+                <a href="#" class="nav-links">About</a>
             </li>
             <li>
-                <a href="#" class="nav-links">Contact Us</a>
+                <a href="#" class="nav-links">Contact</a>
             </li>
             <li>
                 <a href="#" class="nav-links">Blog</a>
