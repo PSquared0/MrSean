3967bcc8f7b8249e671090b8abb8deb79caf8896
Pascale Pierre <pascaleee@gmail.com>
2020-05-31 18:23:48 -0500

    filler copy and box design nnav bar info added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Global from "./styles/global";
+
+import Home from "./views/Home/Home";
+
+import Nav from "./views/Nav/Nav";
+
+const lorem =
+  "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sed iure blanditiis voluptatum nulla quidem minus quam tempora obcaecati necessitatibus inventore! Vitae totam quam pariatur facilis fugit maxime adipisci eaque.";
+
+const data = [
+  {
+    id: Math.random(),
+    title: "Box titulo 1",
+    text: lorem,
+    bgColor: "#D5CAFA"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 2",
+    text: lorem,
+    bgColor: "#EDA9A9"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 3",
+    text: lorem,
+    bgColor: "#F2EE8D"
+  },
+  {
+    id: Math.random(),
+    title: "Box titulo 4",
+    text: lorem,
+    bgColor: "#9FEACD"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Mr Sean's Music
-      </header>
-    </div>
+    <>
+    <Nav />
+      <Global />
+      <Home boxData={data} />
+    </>
   );
 }
 
---
+++ src/views/Nav/Nav.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import { } from "./NavStyles";
+
+class Nav extends Component {
+    render() {
+      return (
+        <nav class="navbar">
+        <a href="#" class="logo">logo</a>
+        <ul class="main-nav" id="js-menu">
+            <li>
+                <a href="#" class="nav-links">Home</a>
+            </li>
+            <li>
+                <a href="#" class="nav-links">Products</a>
+            </li>
+            <li>
+                <a href="#" class="nav-links">About Us</a>
+            </li>
+            <li>
+                <a href="#" class="nav-links">Contact Us</a>
+            </li>
+            <li>
+                <a href="#" class="nav-links">Blog</a>
+            </li>
+        </ul>
+</nav>);
+  
+    }
+  
+    }
+  
+    
+
+
+export default Nav;
